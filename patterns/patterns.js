0a5d348e73f248b3a847a5361230d7eff21a3ac9
Gautham Krishna <gauthamkrishna@Gauthams-MacBook-Pro.local>
2025-11-12 09:21:14 +0530

    moved tests to another file

--- patterns/patterns.js
+++ patterns/patterns.js
@@ -1,11 +1,13 @@
-const FILLED_RECT = "filled-rectangle";
-const HOLLOW_RECT = "hollow-rectangle";
-const ALTERNATE_RECT = "alternating-rectangle";
-const SPACED_ALT_RECT = "spaced-alternating-rectangle";
-const TRIANGLE = "triangle";
-const RIGHT_TRIANGLE = "right-aligned-triangle";
-const DIAMOND = "diamond";
-const HOLLOW_DIAMOND = "hollow-diamond";
+export const styles = {
+    FILLED_RECT: "filled-rectangle",
+    HOLLOW_RECT: "hollow-rectangle",
+    ALTERNATE_RECT: "alternating-rectangle",
+    SPACED_ALT_RECT: "spaced-alternating-rectangle",
+    TRIANGLE: "triangle",
+    RIGHT_TRIANGLE: "right-aligned-triangle",
+    DIAMOND: "diamond",
+    HOLLOW_DIAMOND: "hollow-diamond",
+};
 
 function isZero(dimension) {
     return dimension === 0;
@@ -83,7 +85,6 @@ function triangle(rows) {
     }
 
     return pattern;
-
 }
 
 function rightTriangle(rows) {
@@ -95,19 +96,16 @@ function rightTriangle(rows) {
     }
 
     return pattern;
-
 }
 
 function diamond(rows) {
     const pattern = [];
     const closestOdd = rows % 2 === 0 ? rows - 1 : rows;
 
-    
-
     return pattern;
 }
 
-function generatePattern(style, dimensions) {
+export function generatePattern(style, dimensions) {
     const cols = dimensions[0];
     const rows = dimensions[1];
 
@@ -116,137 +114,21 @@ function generatePattern(style, dimensions) {
     }
 
     switch (style) {
-        case FILLED_RECT:
+        case styles.FILLED_RECT:
             return rectangle(cols, rows).join("\n");
-        case HOLLOW_RECT:
+        case styles.HOLLOW_RECT:
             return hollowRectangle(cols, rows).join("\n");
-        case ALTERNATE_RECT:
+        case styles.ALTERNATE_RECT:
             return alternatingRectangle(cols, rows).join("\n");
-        case SPACED_ALT_RECT:
+        case styles.SPACED_ALT_RECT:
             return spacedAlternatingRec(cols, rows).join("\n");
-        case TRIANGLE:
+        case styles.TRIANGLE:
             return triangle(dimensions).join("\n");
-        case RIGHT_TRIANGLE:
+        case styles.RIGHT_TRIANGLE:
             return rightTriangle(dimensions).join("\n");
-        case DIAMOND:
+        case styles.DIAMOND:
             return diamond(dimensions).join("\n");
         default:
             return "Invalid Style";
     }
 }
-
-function testPattern(message, style, dimensions, expectedValue) {
-    const actualValue = generatePattern(style, dimensions);
-    const isPass = actualValue === expectedValue;
-    let emojiMessage = isPass ? "✅" : "❌";
-    emojiMessage += message;
-
-    const inputMessage = isPass ? '' : `\n  Inputs\t:[${style},${dimensions}]`;
-    const expectedMessage = isPass ? '' : `  Expected\t: \n${expectedValue}`;
-    const actualMessage = isPass ? '' : `\n  Actual\t:\n${actualValue}`;
-    console.log(emojiMessage, inputMessage);
-    console.log(expectedMessage, actualMessage);
-}
-
-function testRectangle() {
-    let expectedPattern = "**\n**\n**\n**";
-    testPattern(" 2 x 4 rectangle", FILLED_RECT, [2, 4], expectedPattern);
-
-    expectedPattern = "*****\n*****\n*****";
-    testPattern(" 5 x 3 rectangle", FILLED_RECT, [5, 3], expectedPattern);
-
-    expectedPattern = "";
-    testPattern(" 0 x 1", FILLED_RECT, [0, 1], expectedPattern);
-}
-
-function testHollowRectangle() {
-    testPattern("0x3 Hrectangle", HOLLOW_RECT, [0, 3], "");
-
-    let expectedPattern = "****\n*  *\n****";
-    testPattern("4x3 Hrectangle", HOLLOW_RECT, [4, 3], expectedPattern);
-
-    expectedPattern = "***\n***";
-    testPattern("3x2 Hrectangle", HOLLOW_RECT, [3, 2], expectedPattern);
-
-    expectedPattern = "*\n*\n*\n*\n*";
-    testPattern("1x5 Hrectangle", HOLLOW_RECT, [1, 5], expectedPattern);
-}
-
-function testAlternatingRectangle() {
-    let message = "3 x 3 Alternating Rectangle";
-    let expectedPattern = "***\n---\n***";
-    testPattern(message, ALTERNATE_RECT, [3, 3], expectedPattern);
-
-    message = "4 x 1 Alternating Rectangle";
-    expectedPattern = "****";
-    testPattern(message, ALTERNATE_RECT, [4, 1], expectedPattern);
-}
-
-function testSpacedAlternating() {
-    let message = "0 x 3 Spaced Alternating Rectangle";
-    let expectedPattern = "";
-    testPattern(message, SPACED_ALT_RECT, [0, 3], expectedPattern);
-
-    message = "3 x 5 Spaced Alternating Rectangle";
-    expectedPattern = "***\n---\n   \n***\n---";
-    testPattern(message, SPACED_ALT_RECT, [3, 5], expectedPattern);
-
-    message = "1 x 3 Spaced Alternating Rectangle";
-    expectedPattern = "*\n-\n ";
-    testPattern(message, SPACED_ALT_RECT, [1, 3], expectedPattern);
-
-    message = "4 x 1 Spaced Alternating Rectangle";
-    expectedPattern = "****";
-    testPattern(message, SPACED_ALT_RECT, [4, 1], expectedPattern);
-
-    message = "4 x 2 Spaced Alternating Rectangle";
-    expectedPattern = "****\n----";
-    testPattern(message, SPACED_ALT_RECT, [4, 2], expectedPattern);
-}
-
-function testRightTriangle() {
-    let message = "5 rows right aligned triangle";
-    let expectedPattern = "    *\n   **\n  ***\n ****\n*****";
-    testPattern(message, RIGHT_TRIANGLE, 5, expectedPattern);
-
-    message = "0 row right aligned triangle";
-    expectedPattern = "";
-    testPattern(message, RIGHT_TRIANGLE, 0, expectedPattern);
-
-    message = "1 row right aligned triangle";
-    expectedPattern = "*";
-    testPattern(message, RIGHT_TRIANGLE, 1, expectedPattern);
-}
-
-function testTriangle() {
-    let message = "5 rows triangle";
-    let expectedPattern = "*\n**\n***\n****\n*****";
-    testPattern(message, TRIANGLE, 5, expectedPattern);
-
-    message = "1 row triangle";
-    expectedPattern = "*";
-    testPattern(message, TRIANGLE, 1, expectedPattern);
-
-    message = "0 row triangle";
-    expectedPattern = "";
-    testPattern(message, TRIANGLE, 0, expectedPattern);
-}
-
-function testDiamond() {
-    let message = "5 rows 💎";
-    let expectedPattern = "  *  \n *** \n******\n *** \n  *  ";
-    testPattern(message, DIAMOND, 5, expectedPattern);
-}
-
-function testAll() {
-    testRectangle();
-    testHollowRectangle();
-    testAlternatingRectangle();
-    testSpacedAlternating();
-    testTriangle();
-    testRightTriangle();
-    testDiamond();
-    
-}
-
-testAll();
---
+++ patterns/test_patterns.js
@@ -0,0 +1,173 @@
+import { generatePattern, styles } from "./patterns.js";
+
+function testPattern(message, fn, args, expectedValue) {
+  const actualValue = fn(...args);
+  const isPass = actualValue === expectedValue;
+  let emojiMessage = isPass ? "✅" : "❌";
+  emojiMessage += message;
+  const inputMessage = isPass ? "" : `\n  Inputs\t:[${args}]`;
+  const expectedMessage = isPass ? "" : `  Expected\t: ${expectedValue}`;
+  const actualMessage = isPass ? "" : `\n  Actual\t: ${actualValue}`;
+  console.log(emojiMessage, inputMessage);
+  console.log(expectedMessage, actualMessage);
+}
+
+const testFn = (testCases) => {
+  testCases.forEach((testCase) =>
+    testPattern(testCase.desc, testCase.fn, testCase.args, testCase.expected)
+  );
+};
+
+function testRectangle() {
+  const testCases = [{
+    desc: "2 x 4 rectangle",
+    fn: generatePattern,
+    args: [styles.FILLED_RECT, [2, 4]],
+    expected: "**\n**\n**\n**",
+  }, {
+    desc: " 5 x 3 rectangle",
+    fn: generatePattern,
+    args: [styles.FILLED_RECT, [5, 3]],
+    expected: "*****\n*****\n*****",
+  }, {
+    desc: " 0 x 1 rectangle",
+    fn: generatePattern,
+    args: [styles.FILLED_RECT, [0, 1]],
+    expected: "",
+  }];
+
+  testFn(testCases);
+}
+
+function testHollowRectangle() {
+  const testCases = [{
+    desc: "0x3 Hrectangle",
+    fn: generatePattern,
+    args: [styles.HOLLOW_RECT, [0, 3]],
+    expected: "",
+  }, {
+    desc: "4x3 Hrectangle",
+    fn: generatePattern,
+    args: [styles.HOLLOW_RECT, [4, 3]],
+    expected: "****\n*  *\n****",
+  }, {
+    desc: "3x2 Hrectangle",
+    fn: generatePattern,
+    args: [styles.HOLLOW_RECT, [3, 2]],
+    expected: "***\n***",
+  }, {
+    desc: "1x5 Hrectangle",
+    fn: generatePattern,
+    args: [styles.HOLLOW_RECT, [1, 5]],
+    expected: "*\n*\n*\n*\n*",
+  }];
+  testFn(testCases);
+}
+
+function testAlternatingRectangle() {
+  const testCases = [{
+    desc: "3 x 3 Alternating Rectangle",
+    fn: generatePattern,
+    args: [styles.ALTERNATE_RECT, [3, 3]],
+    expected: "***\n---\n***",
+  }, {
+    desc: "0 x 3 Alternating Rectangle",
+    fn: generatePattern,
+    args: [styles.ALTERNATE_RECT, [0, 3]],
+    expected: "",
+  }, {
+    desc: "4 x 1 Alternating Rectangle",
+    fn: generatePattern,
+    args: [styles.ALTERNATE_RECT, [4, 1]],
+    expected: "****",
+  }];
+  testFn(testCases);
+}
+
+function testSpacedAlternating() {
+  const testCases = [{
+    desc: "0 x 3 Spaced Alternating Rectangle",
+    fn: generatePattern,
+    args: [styles.SPACED_ALT_RECT, [0, 3]],
+    expected: "",
+  }, {
+    desc: "3 x 5 Spaced Alternating Rectangle",
+    fn: generatePattern,
+    args: [styles.SPACED_ALT_RECT, [3, 5]],
+    expected: "***\n---\n   \n***\n---",
+  }, {
+    desc: "1 x 3 Spaced Alternating Rectangle",
+    fn: generatePattern,
+    args: [styles.SPACED_ALT_RECT, [1, 3]],
+    expected: "*\n-\n ",
+  }, {
+    desc: "4 x 1 Spaced Alternating Rectangle",
+    fn: generatePattern,
+    args: [styles.SPACED_ALT_RECT, [4, 1]],
+    expected: "****",
+  }, {
+    desc: "4 x 2 Spaced Alternating Rectangle",
+    fn: generatePattern,
+    args: [styles.SPACED_ALT_RECT, [4, 2]],
+    expected: "****\n----",
+  }];
+  testFn(testCases);
+}
+
+function testRightTriangle() {
+  const testCases = [{
+    desc: "5 rows right aligned triangle",
+    fn: generatePattern,
+    args: [styles.RIGHT_TRIANGLE, 5],
+    expected: "    *\n   **\n  ***\n ****\n*****",
+  }, {
+    desc: "1 row right aligned triangle",
+    fn: generatePattern,
+    args: [styles.RIGHT_TRIANGLE, 1],
+    expected: "*",
+  }, {
+    desc: "0 row right aligned triangle",
+    fn: generatePattern,
+    args: [styles.RIGHT_TRIANGLE, 0],
+    expected: "",
+  }];
+  testFn(testCases);
+}
+
+function testTriangle() {
+  const testCases = [{
+    desc: "5 rows  triangle",
+    fn: generatePattern,
+    args: [styles.TRIANGLE, 5],
+    expected: "*\n**\n***\n****\n*****",
+  }, {
+    desc: "1 row  triangle",
+    fn: generatePattern,
+    args: [styles.TRIANGLE, 1],
+    expected: "*",
+  }, {
+    desc: "0 row triangle",
+    fn: generatePattern,
+    args: [styles.TRIANGLE, 0],
+    expected: "",
+  }];
+  testFn(testCases);
+}
+
+function testDiamond() {
+  let message = "5 rows 💎";
+  let expectedPattern = "  *  \n *** \n******\n *** \n  *  ";
+  testPattern(message, DIAMOND, 5, expectedPattern);
+}
+
+function testAll() {
+  testRectangle();
+  testHollowRectangle();
+  testAlternatingRectangle();
+  testSpacedAlternating();
+  testTriangle();
+  testRightTriangle();
+  // testDiamond();
+}
+
+testAll();
