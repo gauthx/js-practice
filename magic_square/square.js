ee19f859867da9b6d46cc0e8a704ddd31ab87f02
Gautham Krishna <gauthamkrishna@Gauthams-MacBook-Pro.local>
2025-11-17 14:38:52 +0530

    magic sqr

--- magic_square/square.js
+++ magic_square/square.js
@@ -1,25 +1,3 @@
-const dob = { dd: 5, mm: 5, cc: 20, yy: 25 };
-const formulaes = [
-  (obj) => obj.dd,
-  (obj) => obj.mm,
-  (obj) => obj.cc,
-  (obj) => obj.yy,
-  (obj) => obj.yy + 1,
-  (obj) => obj.cc - 1,
-  (obj) => obj.mm - 3,
-  (obj) => obj.dd + 3,
-  (obj) => obj.mm - 2,
-  (obj) => obj.dd + 2,
-  (obj) => obj.yy + 2,
-  (obj) => obj.cc - 2,
-  (obj) => obj.cc + 1,
-  (obj) => obj.yy - 1,
-  (obj) => obj.dd + 1,
-  (obj) => obj.mm - 1,
-];
-
-const magicSquare = formulaes.map((formulae) => formulae(dob));
-
 const printSquare = (sqr) => {
   return sqr.reduce(
     (grp, x) =>
@@ -30,6 +8,40 @@ const printSquare = (sqr) => {
   ).map((x) => x.join("\t")).join("\n");
 };
 
-console.log(printSquare(magicSquare));
+const takeUserInput = () => {
+  const dobInput = prompt("Enter your date of birth in dd-mm-yyyy order");
+  const dd = parseInt(dobInput.slice(0, 2));
+  const mm = parseInt(dobInput.slice(3, 5));
+  const cc = parseInt(dobInput.slice(6, 8));
+  const yy = parseInt(dobInput.slice(8, 10));
 
+  return { dd, mm, cc, yy };
+};
 
+function main() {
+  const dob = takeUserInput();
+  const formulaes = [
+    (obj) => obj.dd,
+    (obj) => obj.mm,
+    (obj) => obj.cc,
+    (obj) => obj.yy,
+    (obj) => obj.yy + 1,
+    (obj) => obj.cc - 1,
+    (obj) => obj.mm - 3,
+    (obj) => obj.dd + 3,
+    (obj) => obj.mm - 2,
+    (obj) => obj.dd + 2,
+    (obj) => obj.yy + 2,
+    (obj) => obj.cc - 2,
+    (obj) => obj.cc + 1,
+    (obj) => obj.yy - 1,
+    (obj) => obj.dd + 1,
+    (obj) => obj.mm - 1,
+  ];
+
+  const magicSquare = formulaes.map((formulae) => formulae(dob));
+
+  console.log(printSquare(magicSquare));
+}
+
+main();
