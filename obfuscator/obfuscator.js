2c8447e2b0a254de1fb4c6b0bbce2ed38c36d2dc
Gautham Krishna <gauthamkrishna@Gauthams-MacBook-Pro.local>
2025-11-29 20:00:03 +0530

    adds obfuscator

---
+++ obfuscator/obfuscator.js
@@ -0,0 +1,47 @@
+const randomNumBetween = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const breakText = (text, length) => {
+  const brokenText = [];
+  let start = 0;
+  let end = length;
+  while (end <= text.length) {
+    const sliceOfText = text.slice(start, end);
+    const template = `{${sliceOfText},`;
+    brokenText.push(template);
+    start = end;
+    end += length;
+  }
+
+  return brokenText;
+};
+
+const generateGarbageText = (length) => {
+  const chars = ["%", "&", "^", "*", "a", "z", "k", "l"];
+
+  const garbage = [];
+  for (let index = 0; index < length; index++) {
+    const randomIndex = randomNumBetween(0, chars.length - 1);
+    garbage.push(chars[randomIndex]);
+  }
+
+  return garbage;
+};
+
+const insertTextInBetween = (garbage, text) => {
+  return garbage.toSpliced(3, text.length, ...text);
+};
+
+const main = () => {
+  const sentence = "I love apples!";
+  const brokenSentence = breakText(sentence, 3);
+  const obfuscated = [];
+  for (const brokenText of brokenSentence) {
+    const garbageText = generateGarbageText(10);
+    const obfuscatedLine = insertTextInBetween(garbageText, brokenText);
+    obfuscated.push(obfuscatedLine.join(""));
+  }
+  console.log(obfuscated.join("\n"));
+};
+main();
