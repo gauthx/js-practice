3380ca895e57e540f827f333cf1b41ea0a8a67c7
Gautham Krishna <gauthamkrishna@Gauthams-MacBook-Pro.local>
2026-01-05 11:51:47 +0530

    adds merge node

--- huffman_encoding/encoding.js
+++ huffman_encoding/encoding.js
@@ -6,3 +6,32 @@ const calculateFrequencies = (word) => {
   return frequencies;
 };
 
+class Node {
+  constructor(char, freq) {
+    this.char = char;
+    this.freq = freq;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+const createNodes = (frequencies) => {
+  const nodes = [];
+  for (const frequency in frequencies) {
+    nodes.push(new Node(frequency, frequencies[frequency]));
+  }
+  return nodes;
+};
+
+const sortNodes = (nodes) => nodes.sort((a, b) => b.freq - a.freq);
+
+const mergeNodes = (leftChild, rightChild) => {
+  const parentNode = new Node(null, leftChild.freq + rightChild.freq)
+  parentNode.left = leftChild;
+  parentNode.right = rightChild;
+  return parentNode;
+}
+
+const buildTree = (nodes) => {
+
+}
