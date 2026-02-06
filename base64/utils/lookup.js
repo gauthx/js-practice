cf1839a17d70fab0428168a61e765e020508538a
Gautham Krishna <gauthamkrishna@Gauthams-MacBook-Pro.local>
2026-02-06 12:03:08 +0530

    adds lookup table for base-64 encoding

---
+++ base64/utils/lookup.js
@@ -0,0 +1,21 @@
+const createLookup = (lookup, asciiStart, asciiEnd, mappedVal) => {
+  for (let ascii = asciiStart; ascii <= asciiEnd; ascii++) {
+    const char = String.fromCharCode(ascii);
+    lookup[char] = mappedVal++;
+  }
+  return lookup;
+};
+
+const main = () => {
+  const lookupTable = {};
+  createLookup(lookupTable, 65, 90, 0); // uppercase letters mapping
+  createLookup(lookupTable, 97, 122, 26); // lowercase letters mapping
+  createLookup(lookupTable, 48, 57, 52); // 0-9 mapping
+  lookupTable[62] = "+";
+  lookupTable[63] = "/";
+  console.log(lookupTable);
+
+  Deno.writeTextFileSync("./data/lookup_table.json", JSON.stringify(lookupTable));
+};
+
+main();
