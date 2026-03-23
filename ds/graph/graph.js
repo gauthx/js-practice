e4467c415767b4e9740044309cb43bb5e2d52085
Gautham Krishna <gauthamkrishna@Gauthams-MacBook-Pro.local>
2026-03-23 23:07:41 +0530

    adds bfs traversal and changes verices to stations

--- ds/graph/graph.js
+++ ds/graph/graph.js
@@ -4,25 +4,57 @@ const addNode = (adjacenyList, word) => {
 
 const addEdge = (adjacenyList, word1, word2) => {
   adjacenyList.get(word1).push(word2);
-  adjacenyList.get(word2).push(word1);
+};
+
+const bfs = (adjacenyList, start) => {
+  const visited = new Set();
+
+  const queue = [start];
+
+  while (queue.length > 0) {
+    const station = queue.shift();
+    const destinations = adjacenyList.get(station);
+
+    for (const destination of destinations) {
+      console.log(`Visiting ${destination} from ${station}`);
+
+      if (!visited.has(destination)) {
+        visited.add(destination);
+        queue.push(destination)
+      }
+    }
+  }
 };
 
 const main = () => {
-  const words = ["cat", "bat", "bet", "bed", "cot", "cog", "dog"];
+  const stations = [
+    "Chicago",
+    "Omaha",
+    "Pittsburgh",
+    "Kansas City",
+    "Saint Louis",
+  ];
 
-  const rhymingWords = [
-    ["cat", "bat"],
-    ["bat", "bet"],
-    ["bet", "bed"],
-    ["cat", "cot"],
-    ["cot", "cog"],
-    ["cog", "dog"],
+  const routes = [
+    ["Chicago", "Omaha"],
+    ["Chicago", "Saint Louis"],
+    ["Chicago", "Pittsburgh"],
+    ["Omaha", "Kansas City"],
+    ["Pittsburgh", "Saint Louis"],
+    ["Kansas City", "Omaha"],
+    ["Kansas City", "Saint Louis"],
+    ["Saint Louis", "Kansas City"],
+    ["Saint Louis", "Chicago"],
+    ["Saint Louis", "Pittsburgh"],
   ];
 
   const adjacenyList = new Map();
-  words.forEach((word) => addNode(adjacenyList, word));
-  rhymingWords.forEach((rhymingPair) => addEdge(adjacenyList, ...rhymingPair));
+  stations.forEach((word) => addNode(adjacenyList, word));
+  routes.forEach((connectedStation) =>
+    addEdge(adjacenyList, ...connectedStation),
+  );
   console.log(adjacenyList);
+  bfs(adjacenyList, "Chicago");
 };
 
 main();
